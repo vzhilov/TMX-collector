6d2f07f6074e8b5c42be70f386bd8fba67b9aac1
root <root@wdmc.lan>
2020-01-29 17:23:27 +0300

    First part for db connection

--- tmxCollector.php
+++ tmxCollector.php
@@ -1,7 +1,19 @@
 <?php
 
-echo "Hello Word!"
+$tmxDir = "/home/";
 
+$tmx_db = mysqli_connect('localhost', 'tmx', 'mypass', 'tmx');
 
+//mysqli_query($tmx_db, 'CREATE TEMPORARY TABLE `table`');
+
+$files = glob($tmxDir.'*.txt');
+
+//var_dump($files);
+
+foreach ($files as $file) {
+
+echo strtolower(substr($file, -7, 3))."\r\n";
+
+}
 
 ?>
