de293539fe4d0c052f38c0b15bb14fae27dcd458
vzhilov <vzhilov@gmail.com>
2020-01-30 01:14:58 +0300

    Update tmxCollector.php

--- tmxCollector.php
+++ tmxCollector.php
@@ -4,16 +4,39 @@ $tmxDir = "/home/";
 
 $tmx_db = mysqli_connect('localhost', 'tmx', 'mypass', 'tmx');
 
-//mysqli_query($tmx_db, 'CREATE TEMPORARY TABLE `table`');
-
-$files = glob($tmxDir.'*.txt');
+$ruFiles = glob($tmxDir.'*_RU.txt');
 
 //var_dump($files);
 
-foreach ($files as $file) {
+foreach ($ruFiles as $ruFile) {
 
-echo strtolower(substr($file, -7, 3))."\r\n";
-
+//$fileLang = strtolower(substr($file, -7, 3));
+$engFile = substr($ruFile, -7)."_EN.txt";
+if (file_exists($engFile)) {
+  $ruText=file_get_contents($ruFile);
+  $engText = file_get_contents($engFile);
+  
+  $ruSentencesArray = breakDown2Sentences($ruText);
+  $engSentencesArray = breakDown2Sentences($engText);
+}
+    
+    
+    
+    
+    
+    
+    
+function breakDown2Sentences($fullText) {
+  $sentecesArray = array();
+  $paragraphs = array_filter(explode("\n", $fullText);
+  foreach ($paragraphs as $paragraph) {
+    if (strpos(".", $paragraph)) {
+      $sentences = array_filter(explode(".", $paragraph));
+      foreach ($sentences as $sentence) {
+        $sentenceArray[] = trim($sentence);
+      }
+    } else $sentenceArray[] = trim($paragraph);
+  }
 }
 
 ?>
