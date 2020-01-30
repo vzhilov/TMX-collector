1de311c7f457dcb1f1d221b59f6d329544b5b4dd
root <root@wdmc.lan>
2020-01-30 17:56:01 +0300

    Chase up

--- tmxCollector.php
+++ tmxCollector.php
@@ -8,45 +8,42 @@ $ruFiles = glob($tmxDir.'*_[rR][uU].[tT][xX][tT]');
 
 foreach ($ruFiles as $ruFile) {
 
-	$enFile = substr($ruFile, -7)."_EN.txt";
+	$enFile = substr($ruFile, 0, -7)."_en.txt";
 	if (file_exists($enFile)) {
 	  $ruText=file_get_contents($ruFile);
 	  $enText = file_get_contents($enFile);
 	  
 	  $ruSentencesArray = breakDown2Sentences($ruText);
 	  $enSentencesArray = breakDown2Sentences($enText);
-	  
 	  //Then we insert to SQL
-	  
-	  
-	  $stmtSelect = $tmx_db->prepare("SELECT id FROM 'tmx' where `source` = ?;");
-	  $stmtSelect->bind_param("s", $ruSentence); 
-	  
-	  $stmtInsert = $tmx_db->prepare("INSERT INTO 'tmx' ( `direction`, `source` , `target`, `count` ) VALUES ( ?, ?, ?, ? );");
-	  $stmt->bind_param("issi", $direction, $ruSentence, $enSentence, $count);
-	  
-	  $stmtInsert = $tmx_db->prepare("UPDATE 'tmx' set `count` = `count`+1 WHERE id = ?;");
-	  $stmtInsert->bind_param("i", $id);
-
 	  
 	  $direction = 0;
 	  $count = 1;
 	  $i = 0;
+	  $ruSentence = "";
+	  $enSentence = "";
 	  
+	  $stmtSelect = $tmx_db->prepare("SELECT id FROM `tmx` where `source` = ?;");
+	  $stmtSelect->bind_param("s", $ruSentence); 
 	  
+	  $stmtInsert = $tmx_db->prepare("INSERT INTO `tmx` ( `direction`, `source`, `target`, `count` ) VALUES ( ?, ?, ?, ? );");
+//echo $tmx_db->error;
+	  $stmtInsert->bind_param("issi", $direction, $ruSentence, $enSentence, $count);
+	  
+	  $stmtUpdate = $tmx_db->prepare("UPDATE `tmx` SET `count` = `count`+1 WHERE id = ?;");
+	  $stmtUpdate->bind_param("i", $id);
 
-
-	  foreach ($ruSentenceArray as $ruSentence) {
-		$enSentence = $enSentencesArray[i];
+	  foreach ($ruSentencesArray as $ruSentence) {
+		$enSentence = $enSentencesArray[$i];
 		
-		$stmSelect->execute();
-		$stmSelect->store_result();
-		if ($stmSelect->num_rows == 0) {
+		$stmtSelect->execute();
+		$stmtSelect->store_result();
+		if ($stmtSelect->num_rows == 0) {
 		  $stmtInsert->execute();
 		} else {
 		  $row = $stmtSelect->fetch(); 
 		  $id = $row['id'];
-		  $stmUpdate->execute();
+		  $stmtUpdate->execute();
 		}
 		$i++;
 	  }
@@ -63,16 +60,19 @@ foreach ($ruFiles as $ruFile) {
     
     
 function breakDown2Sentences($fullText) {
+  $fullText = mb_convert_encoding($fullText, 'UTF-8');
   $sentecesArray = array();
   $paragraphs = array_filter(explode("\n", $fullText));
   foreach ($paragraphs as $paragraph) {
     if (strpos(".", $paragraph)) {
       $sentences = array_filter(explode(".", $paragraph));
       foreach ($sentences as $sentence) {
-        $sentenceArray[] = trim($sentence);
+        $sentencesArray[] = trim($sentence);
       }
-    } else $sentenceArray[] = trim($paragraph);
+    } else $sentencesArray[] = trim($paragraph);
   }
+
+  return $sentencesArray;
 }
 
 ?>
